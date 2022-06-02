6d4db9769756ab241416bb1133d3cc425ee8546d
YulianSan <knoob0733@gmail.com>
2022-06-02 17:21:41 -0300

    Adicionando a animação de pulo e a imagem de terra

--- mario/draw.mario.js
+++ mario/draw.mario.js
@@ -1,14 +1,9 @@
 const mario = new Image();
 mario.src="./mario/mario.png"
 var frames=0;
-const velAnimacao = 10;
-// 
-//     {
-//         x:9,    y:105,     w:45-9,      h:141-105
-//     },
-//     {
-//         x:74,    y:105,     w:110-74,      h:141-105
-//     }
+const velAnimacaoAndando = 10;
+const velAnimacaoPulando = 50;
+
 const framesAndando = [
     {
         x:13,    y:17,     w:40-13,      h:51-17
@@ -18,23 +13,49 @@ const framesAndando = [
     }
 ];
 
+const framesPulando = [
+    {
+        x:9,    y:105,     w:45-9,      h:141-105
+    },
+    {
+        x:74,    y:105,     w:110-74,      h:141-105
+    },
+    {
+        x:205,    y:17,     w:241-205,      h:53-17
+    }
+]
 function drawMario(ctx) {
     frames++;
-    let indice = Math.floor( (frames%( velAnimacao*framesAndando.length )) /velAnimacao );
+    let indiceA = Math.floor( (frames%( velAnimacaoAndando*framesAndando.length )) /velAnimacaoAndando );
+    let indiceP = ( velAnimacaoPulando * framesPulando.length ) <= frames 
+    ? framesPulando.length-1 
+    : Math.floor( (frames%( velAnimacaoPulando*framesPulando.length )) /velAnimacaoPulando );
     
     
-    
+
     ctx.drawImage(
         mario,
-        framesAndando[indice].x,
-        framesAndando[indice].y,
-        framesAndando[indice].w,
-        framesAndando[indice].h,
+        framesAndando[indiceA].x,
+        framesAndando[indiceA].y,
+        framesAndando[indiceA].w,
+        framesAndando[indiceA].h,
         100,
         450,
-        framesAndando[indice].w,
-        framesAndando[indice].h
-        ); 
+        framesAndando[indiceA].w,
+        framesAndando[indiceA].h
+        );
+
+    ctx.drawImage(
+        mario,
+        framesPulando[indiceP].x,
+        framesPulando[indiceP].y,
+        framesPulando[indiceP].w,
+        framesPulando[indiceP].h,
+        200,
+        450,
+        framesPulando[indiceP].w,
+        framesPulando[indiceP].h
+        );  
 }
 
 export default { drawMario };
